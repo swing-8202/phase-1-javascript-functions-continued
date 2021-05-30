77e0d37068548a182a1283db4b02d76556ac2329
Sophie <swing.sophie.8202@gmail.com>
2021-05-30 12:59:56 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity1 = 'go to the office') {
+    return `This Monday, I will ${activity1}.`
+}
+
+// describe("defines wrapAdjective function according to the specification", function() {
+//     it("function exists", function() {
+//       expect(wrapAdjective).to.exist
+//     })
+    
+//     it("when initialized with '*' creates a function that, when called, wraps an adjective in a highlight", function() {
+//       let result = wrapAdjective()
+//       let emphatic = result("a hard worker")
+//       expect(emphatic).to.equal("You are *a hard worker*!")
+//     });
+
+//     it("when initialized with '||' creates a function that, when called, wraps an adjective in a highlight", function() {
+//       let result = wrapAdjective("||")
+//       let emphatic = result("a dedicated programmer")
+//       expect(emphatic).to.equal("You are ||a dedicated programmer||!")
+//     });
+
+// function wrapAdjective(flair = '*') {
+//     const greeeting = function(adjective = 'special') {
+//         return `You are ${flair}${adjective}${flair}!`;
+//     }
+//     return greeting;
+// }
+
+function wrapAdjective(flair="*") {
+    return function(adjective="special") {
+      return `You are ${flair}${adjective}${flair}!`
+    }
+  }
